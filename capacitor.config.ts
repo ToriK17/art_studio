effde826b09390050c50c23194161de330c67f33
ToriK17 <vfcad@protonmail.com>
2024-02-25 09:08:37 -0700

    update config for live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.kafatiartstudio.kafati_art_studio',
   appName: 'kafati_art_studio',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: '9d450253',
+      channel: 'Production',
+      autoUpdateMethod: 'background'
+    }
   }
 };
 
